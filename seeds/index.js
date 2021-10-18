9f687be7a321ce2565d76a7210b225a839b0c653
Catherine Pope <me@catherinepope.com>
2021-10-18 18:58:44 +0100

    Added images and styling

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
    for (let i = 0; i < 50; i++) {
        const random1000 = Math.floor(Math.random() * 1000);
+       const price = Math.floor(Math.random() * 20) + 10;
        const camp = new Campground({
            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-           title: `${sample(descriptors)} ${sample(places)}`
+           title: `${sample(descriptors)} ${sample(places)}`,
+           image: 'https://source.unsplash.com/collection/483251',
+           description: 'LoLorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt pulvinar sagittis. Donec posuere, odio sed imperdiet dignissim, nisl lorem ullamcorper nulla, non lacinia mauris mi at arcu. Etiam ornare ultricies lobortis. Sed pharetra purus at magna finibus, vitae molestie diam dignissim. In rutrum tristique erat sit amet accumsan. Nulla id consequat enim, a mattis justo. Curabitur fermentum tempor ex, eu pellentesque sem dapibus eget.',
+           price
        })
        await camp.save();
    }
