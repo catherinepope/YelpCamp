c15b48f87939e1a21a62ab37a2651728709a7b17
Catherine Pope <me@catherinepope.com>
2021-10-22 17:23:24 +0100

    Added registration form and validation

--- app.js
+++ app.js
@@ -10,8 +10,10 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/reviews');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/reviews');
+
 
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -68,8 +70,9 @@ app.get('/fakeUser', async (req, res) => {
     res.send(newUser);
 });
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
