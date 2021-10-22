3a60da9a09421ef142e7615302e5e956e613e9fd
Catherine Pope <me@catherinepope.com>
2021-10-22 14:22:04 +0100

    Added error flash messages

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -33,6 +33,10 @@ router.post('/', validateCampground, catchAsync(async (req, res, next) => {
 
 router.get('/:id', catchAsync(async(req, res) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if(!campground) {
+        req.flash('error', 'Campground not found');
+        return res.redirect('/campgrounds');
+    }
     res.render('campgrounds/show', { campground });
 }));
 
