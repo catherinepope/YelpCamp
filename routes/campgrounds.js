5adc9bdc9f61b98e4b8564acf9e86bd3d89e37de
Catherine Pope <me@catherinepope.com>
2021-10-25 16:47:48 +0100

    Refactored user and campground routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,23 +1,38 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const campgrounds = require('../controllers/campgrounds');
-const catchAsync = require('../utilities/catchAsync');
-const { isLoggedIn, isAuthor, validateCampground } = require('../middleware'); 
+const campgrounds = require("../controllers/campgrounds");
+const catchAsync = require("../utilities/catchAsync");
+const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 
-const Campground = require('../models/campground');
+const Campground = require("../models/campground");
 
-router.get('/', catchAsync(campgrounds.index));
+router
+  .route("/")
+  .get(catchAsync(campgrounds.index))
+  .post(
+    isLoggedIn,
+    validateCampground,
+    catchAsync(campgrounds.createCampground)
+  );
 
-router.get('/new', isLoggedIn, campgrounds.renderNewForm);
+router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+router
+  .route("/:id")
+  .get(catchAsync(campgrounds.showCampground))
+  .put(
+    isLoggedIn,
+    isAuthor,
+    validateCampground,
+    catchAsync(campgrounds.updateCampground)
+  )
+  .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
-router.get('/:id', catchAsync(campgrounds.showCampground));
-
-router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditCampground));
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
+router.get(
+  "/:id/edit",
+  isLoggedIn,
+  isAuthor,
+  catchAsync(campgrounds.renderEditCampground)
+);
 
 module.exports = router;
